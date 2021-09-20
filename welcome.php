7eb629a16c4e8c2ae0c8f9d47083ebf931889ce5
deckardcheung <33850004+deckardcheung@users.noreply.github.com>
2021-09-20 10:43:44 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -7,7 +7,7 @@
 <?php echo "This Demo is using"; ?><br>
 <?php echo "Pipeline: AWS CodePipeline"; ?><br>
 <?php echo "Source: Github, repo @ source"; ?><br>
-<?php echo "Build: AWS CodeBuild, Artifacts @ S3"; ?><br>
+<?php echo "Build: AWS CodeBuild, Artifacts @ Codepipeline default S3"; ?><br>
 <?php echo "Deploy Provider: AWS Elastic Beanstalk, Input Artifacts @ BuildArtifact"; ?> 
 </center>
 </body>
